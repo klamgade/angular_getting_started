8228c86c3a431f14b799d9297a3bbe7fe7f72047
kamal <lamgadekamal@gmail.com>
2020-03-13 22:53:09 +1300

    Routing and Navigation Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductsComponent } from './product-module/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
